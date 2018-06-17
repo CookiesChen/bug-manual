2d2570f3b251ee57b1dea0b1e4285049b21faf1c
CookiesChen <1109349604@qq.com>
2018-06-17 12:27:24 +0800

    change

---
+++ src/components/MainPage.vue
@@ -0,0 +1,40 @@
+<template>
+  <el-row>
+  <el-col :span="24"><div class="grid-content bg-purple-dark"></div></el-col>
+  </el-row>
+</template>
+
+<script>
+export default {
+  name: 'MainPage',
+  data () {
+    return {
+      radio: '1'
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+  .el-col {
+    border-radius: 4px;
+  }
+  .bg-purple-dark {
+    background: #99a9bf;
+  }
+  .bg-purple {
+    background: #d3dce6;
+  }
+  .bg-purple-light {
+    background: #e5e9f2;
+  }
+  .grid-content {
+    border-radius: 4px;
+    min-height: 36px;
+  }
+  .row-bg {
+    padding: 10px 0;
+    background-color: #f9fafc;
+  }
+</style>
